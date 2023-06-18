55aedac186c93140188d9e2006d41695243348b3
Chirag <chiragsonar1718@gmail.com>
2023-06-18 13:32:25 +0530

    feat: decalred user & loading state, passed state as props to navbar and usergrid. Did conditional rendering of loading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import UserGrid from "./components/UserGrid/UserGrid";
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
   return (
     <div className="App">
-      Hell world
+      <Navbar setUsers={setUsers} setLoading={setLoading} />
+      {loading ? <span className="loader">Loading</span> 
+       : <UserGrid users={users} />}
     </div>
   );
 }
