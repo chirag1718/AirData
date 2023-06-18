bdb2fedb72b9fd2db627b29309bfef4287e63703
Chirag <chiragsonar1718@gmail.com>
2023-06-18 13:51:10 +0530

    feat: added a loader from loading.io

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import "./loader.css";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div class="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
