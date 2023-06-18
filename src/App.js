886c090678fed754e1be9b8c23340fa996f98424
Chirag <chiragsonar1718@gmail.com>
2023-06-18 13:50:24 +0530

    added a loader component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import UserGrid from "./components/UserGrid/UserGrid";
+import Loader from "./components/Loader/Loader";
 function App() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   return (
     <div className="App">
       <Navbar setUsers={setUsers} setLoading={setLoading} />
-      {loading ? <span className="loader">Loading</span> 
-       : <UserGrid users={users} />}
+      {loading ? <Loader /> : <UserGrid users={users} />}
     </div>
   );
 }
